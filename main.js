a2a69205c61b05d907755438a3c895a1c52a641e
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-09-07 15:27:39 +0600

    Add files via upload
    Ternary operator in JavaScript

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var registerPassword = 'abc123';
+
+var loginPassword = 'abc123';
+
+var output = registerPassword === loginPassword ? 'correct passWord' : 'wrong password';
+
+
+console.log(output);
